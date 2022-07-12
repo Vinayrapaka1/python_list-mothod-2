23409b4674b4fa83ddd1a2cfd2fc675568e5a8a5
VINAY KUMAR RAPAKA <107675897+Vinayrapaka1@users.noreply.github.com>
2022-07-12 17:47:24 +0530

    Add files via upload

---
+++ list_method.py
@@ -0,0 +1,59 @@
+# v=[1,3,4,5,6,7,8]
+# v.insert(1,'kazipet')
+# v.insert(-1,'vinay')
+# v.insert(4,23)
+# print(v)
+
+# a=[12,13,14,15,16]
+# print(a.pop(1))
+# a.pop(1)
+# print(a)
+# a.pop(2)
+# print(a)
+# a.pop(12)
+# print(a)
+# a.pop(7)
+# print(a)
+
+# n=[1,2,3,'hello',4,5,6]
+# n.remove('hello')
+# print(n)
+# n.remove(5)
+# print(n)
+# n.remove(2)
+# print(n)
+
+# a=[1,2,3,4,5,6,7]
+# print(a[::-1])
+# a.reverse()
+# print(a)
+
+from pickle import TRUE
+
+
+# v=[3,4,2,5,1,6]
+# print(*v)
+# v.sort()
+# print(v)
+# v.sort(reverse=True)
+# print(v)
+# v.sort(reverse=False)
+# print(v)
+
+# a=[6,4,7,3,8,1,2]
+# a.sort()
+# print(a)
+
+# print(chr(65))
+# print(chr(68))
+# print(chr(90))
+# print(chr(97))
+# print(chr(122))
+# print(ord('A'))
+# print(ord('v'))
+# print(ord('Z'))
+# print(ord('Z'))
+
+# v=['VI','es','ak','NA','AZ']
+# v.sort()
+# print(v)
